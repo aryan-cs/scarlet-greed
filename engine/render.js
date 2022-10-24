a5daccdde23d2d2952d376868eb95a3f3a46ea60
aryan-cs <aryan05g@gmail.com>
2022-10-23 20:41:46 -0500

    started render class

---
+++ engine/render.js
@@ -0,0 +1,35 @@
+class Renderer {
+
+    constructor (spritesheet, spriteWidth, spriteHeight, spriteCount, frameDelay, loop, scale) {
+
+        this.spritesheet = spritesheet;
+        this.spriteWidth = spriteWidth;
+        this.spriteHeight = spriteHeight;
+        this.spriteCount = spriteCount;
+        this.frameDelay = frameDelay;
+        this.loop = loop;
+        this.scale = scale;
+
+    }
+
+    display (state) {
+
+        if (state === "idle") {
+
+        }
+
+        if (state === "walk") {
+            
+        }
+        
+        if (state === "die") {
+
+        }
+
+        return frame;
+
+    }
+    
+    
+
+  }
